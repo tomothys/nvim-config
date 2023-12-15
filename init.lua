32873b2100826d02a2cffc606d2906306999811b
Thomas Schmieck <mail@schmieck.de>
2023-12-15 09:57:05 +0100

    Remove treesitter folding

--- init.lua
+++ init.lua
@@ -91,9 +91,6 @@ require("lazy").setup({
                 highlight = { enable = true, additional_vim_regex_highlighting = false },
                 indent = { enable = true }
             })
-
-            vim.opt.foldmethod = "expr"
-            vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
         end
     },
     {
